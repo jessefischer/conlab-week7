79ad786bd3851717d22b9a694a0404ed7693d148
Jesse Fischer <59373483+jessefischer@users.noreply.github.com>
2024-10-19 13:59:51 -0400

    Mock responses

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 const ANIMATION_DELAY_THRESHOLD = 50;
 
 let appState = "preload"; // "preload" | "intro" | "idle" |
+let responses = [];
 
 window.addEventListener("load", () => {
   renderApp();
@@ -27,7 +28,7 @@ const renderApp = () => {
     setTimeout(() => {
       appState = "transition";
       renderApp();
-    }, 3000);
+    }, 2500);
     return;
   }
   if (appState === "transition") {
@@ -46,10 +47,13 @@ const renderApp = () => {
       messageEl.classList.add("visible");
       setTimeout(() => {
         const inputEl = document.createElement("input");
+        inputEl.id = "input";
         const submitEl = document.createElement("button");
         submitEl.type = "submit";
         submitEl.innerHTML = "Submit";
+        submitEl.id = "submitButton";
         const inputContainerEl = document.createElement("div");
+        inputContainerEl.id = "inputContainer";
         inputContainerEl.classList.add("inputContainer");
         inputContainerEl.appendChild(inputEl);
         inputContainerEl.appendChild(submitEl);
@@ -59,8 +63,50 @@ const renderApp = () => {
           appState = "waitingForInput";
           renderApp();
         }, ANIMATION_DELAY_THRESHOLD);
-      }, 5000);
+      }, 2500);
     }, ANIMATION_DELAY_THRESHOLD);
     return;
+  }
+  if (appState === "waitingForInput") {
+    const submitButtonEl = document.getElementById("submitButton");
+    submitButtonEl.onclick = async () => {
+      const inputButtonEl = document.getElementById("input");
+      const input = inputButtonEl.value;
+      const response = await fetch("submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ input }),
+      });
+      responses = await response.json();
+      appState = "renderingResponses";
+      renderApp();
+    };
+    return;
+  }
+  if (appState === "renderingResponses") {
+    const messageEl = document.getElementById("message");
+    const inputContainerEl = document.getElementById("inputContainer");
+    [messageEl, inputContainerEl].forEach((el) =>
+      el.classList.remove("visible")
+    );
+    setTimeout(() => {
+      [messageEl, inputContainerEl].forEach((el) => el.remove());
+      const responseContainerEl = document.createElement("div");
+      responseContainerEl.classList.add("responseContainer");
+      responses.forEach((response) => {
+        const responseEl = document.createElement("div");
+        responseEl.classList.add("response");
+        responseEl.innerHTML = response;
+        const scale = Math.random() + 0.5;
+        const x = (Math.random() - 0.5) * window.innerWidth;
+        const y = (Math.random() - 0.5) * window.innerHeight;
+        responseEl.style.transform = `translateX(${x}px) translateY(${y}px) scale(${scale})`;
+        console.log(responseEl.style.transform);
+        responseContainerEl.appendChild(responseEl);
+      });
+      appEl.appendChild(responseContainerEl);
+    }, 2000);
   }
 };
