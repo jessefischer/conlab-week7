bf7020ba6fe9f4d9b19827dfcaa10661c53b1ad9
Jesse Fischer <59373483+jessefischer@users.noreply.github.com>
2024-10-19 13:06:42 -0400

    App setup and basic styling

--- public/app.js
+++ public/app.js
@@ -1,15 +1,66 @@
 const ANIMATION_DELAY_THRESHOLD = 50;
 
-let appState = "preload";
+let appState = "preload"; // "preload" | "intro" | "idle" |
 
 window.addEventListener("load", () => {
-  const appEl = document.getElementById("app");
-  const messageEl = document.createElement("div");
-  messageEl.innerHTML = "Think of someone special in your life.";
-  messageEl.classList.add("message");
-  appEl.appendChild(messageEl);
-  setTimeout(
-    () => messageEl.classList.add("visible"),
-    ANIMATION_DELAY_THRESHOLD
-  );
+  renderApp();
 });
+
+const renderApp = () => {
+  console.log({ appState });
+  const appEl = document.getElementById("app");
+  if (appState === "preload") {
+    const messageEl = document.createElement("div");
+    messageEl.id = "message";
+    messageEl.innerHTML = "Think of someone special in your life.";
+    messageEl.classList.add("message");
+    appEl.appendChild(messageEl);
+    setTimeout(
+      () => messageEl.classList.add("visible"),
+      ANIMATION_DELAY_THRESHOLD
+    );
+    appState = "intro";
+    renderApp();
+    return;
+  }
+  if (appState === "intro") {
+    setTimeout(() => {
+      appState = "transition";
+      renderApp();
+    }, 3000);
+    return;
+  }
+  if (appState === "transition") {
+    const messageEl = document.getElementById("message");
+    messageEl.classList.remove("visible");
+    setTimeout(() => {
+      appState = "question";
+      renderApp();
+    }, 2000);
+    return;
+  }
+  if (appState === "question") {
+    const messageEl = document.getElementById("message");
+    messageEl.innerHTML = "What would you say to them if you weren’t scared?";
+    setTimeout(() => {
+      messageEl.classList.add("visible");
+      setTimeout(() => {
+        const inputEl = document.createElement("input");
+        const submitEl = document.createElement("button");
+        submitEl.type = "submit";
+        submitEl.innerHTML = "Submit";
+        const inputContainerEl = document.createElement("div");
+        inputContainerEl.classList.add("inputContainer");
+        inputContainerEl.appendChild(inputEl);
+        inputContainerEl.appendChild(submitEl);
+        messageEl.parentNode.appendChild(inputContainerEl);
+        setTimeout(() => {
+          inputContainerEl.classList.add("visible");
+          appState = "waitingForInput";
+          renderApp();
+        }, ANIMATION_DELAY_THRESHOLD);
+      }, 5000);
+    }, ANIMATION_DELAY_THRESHOLD);
+    return;
+  }
+};
